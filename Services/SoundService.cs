e387513fe7a08c8e76e150dcb151f6518775352c
Balaji Manokaran <praveshbalaji144@gmail.com>
2026-09-02 20:54:14 +0530

    fix: optimize iOS simulator publish step for Appetize.io and add CodesignRequireProvisioningProfile override

--- Services/SoundService.cs
+++ Services/SoundService.cs
@@ -3,6 +3,10 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.Maui.Storage;
+#if IOS || MACCATALYST
+using Foundation;
+using AVFoundation;
+#endif
 
 namespace FuelRushMaui.Services
 {
